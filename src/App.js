4fa6263497c1617848eb7a73596237e743bb388c
AshishLNadoda <ashishnadoda007@gmail.com>
2023-06-12 18:59:22 +0530

    upload demo changes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
 
 
-<<<<<<< HEAD
 
 import Nav from './components/weeklytest/nav/Nav'
 import Footer from './components/weeklytest/footer/Footer'
 
 
-=======
-import Bpp from './components/todo/Todo';
-import "./App.css";
->>>>>>> f9a73eae97d458e983f417b19f578ba09f8dc95a
+
+
 
 function App() {
   
   return (
     <div className="App">
-<<<<<<< HEAD
+
       <Nav/>
     <Footer/>
 
-=======
-      <Bpp/>
->>>>>>> f9a73eae97d458e983f417b19f578ba09f8dc95a
+
+   
      
 
      
