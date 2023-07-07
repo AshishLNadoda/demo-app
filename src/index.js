e35b7b9c563c0bcd57c27d7d9068c590bd1a9e8f
AshishLNadoda <ashishnadoda007@gmail.com>
2023-07-07 21:49:51 +0530

    last

--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 
-// redux
-import store from "./components/test2july/redux/store";
-import { Provider } from "react-redux";
+
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  
+  
+
   document.getElementById("root")
 );
