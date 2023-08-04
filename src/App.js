4038bdec398f2e9046b0b5ea6d1008bca6799e19
AshishLNadoda <ashishnadoda007@gmail.com>
2023-08-04 21:01:51 +0530

    001

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import React from "react"
-import ImageCard from "./MCT/ImageCard"
-import "./App.css"
+ import Heading from "./fs-06-/001/Heading"
 
 
 
@@ -9,14 +7,7 @@ import "./App.css"
 function App() {
   return (
     
-    <div className="main">
-     
-       <img
-    class="demo-bg"
-    src="https://i0.wp.com/www.printmag.com/wp-content/uploads/2021/02/4cbe8d_f1ed2800a49649848102c68fc5a66e53mv2.gif?resize=476%2C280&ssl=1">
-    </img>
-    <ImageCard />
-    </div>
+   <Heading/>
       
    
   )
